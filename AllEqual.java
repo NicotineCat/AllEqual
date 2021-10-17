256785537a376b5d3002a2f08db9678be6768764
EthanNicotine <escamezvalentin@gmail.com>
2021-10-17 12:25:07 +0200

    Create AllEqual.java

---
+++ AllEqual.java
@@ -0,0 +1,10 @@
+public class AllEqual{
+    public static void main(String[] args){
+        if (args[0] == args[1] && args[1] == args[2]){
+            System.out.println("equal");
+        }
+        else{
+                System.out.println("not equal"); 
+            }
+    }
+}
