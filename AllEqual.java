822143844a1e71f77eb90a9a2e515a749cc9745c
EthanNicotine <escamezvalentin@gmail.com>
2021-10-17 13:07:20 +0200

    Update AllEqual.java
    Comentarios agregados y cambio de nombre de variable parseada para evitar confusiones

--- AllEqual.java
+++ AllEqual.java
@@ -1,10 +1,11 @@
 public class AllEqual{
     public static void main(String[] args){
-        System.out.println(args[0] + args[1] + args[2]);
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int c = Integer.parseInt(args[2]);
-        if (a == b && a == c)
+        //Parseo los args a int
+        int numero1 = Integer.parseInt(args[0]);
+        int numero2 = Integer.parseInt(args[1]);
+        int numero3 = Integer.parseInt(args[2]);
+        //Si numero1 es igual a n2 y n1 es igual a n3 entonces true
+        if (numero1 == numero2 && numero1 == numero3)
             System.out.println("equal");
         else
             System.out.println("not equal"); 
