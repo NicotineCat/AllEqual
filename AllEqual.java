35f2927eb0aa03f977324480151e421eefd769be
EthanNicotine <escamezvalentin@gmail.com>
2021-10-17 13:05:02 +0200

    Update AllEqual.java
    No he logrado que compare los Strings así que los parseé a números

--- AllEqual.java
+++ AllEqual.java
@@ -1,10 +1,12 @@
 public class AllEqual{
     public static void main(String[] args){
-        if (args[0] == args[1] && args[1] == args[2]){
+        System.out.println(args[0] + args[1] + args[2]);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        if (a == b && a == c)
             System.out.println("equal");
-        }
-        else{
-                System.out.println("not equal"); 
-            }
+        else
+            System.out.println("not equal"); 
     }
 }
